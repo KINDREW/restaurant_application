06e8b6429d7e6d2b248a82a5a43d9a2abf829685
kindrew <kindrew99@gmail.com>
2023-10-28 22:29:49 +0000

    update

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,17 +4,21 @@ import { useEffect, useRef } from "react";
 
 const Navbar = () => {
   const targetRef = useRef(null);
+  const targetRef2 = useRef(null);
 
   const showMenu = () => {
     const navMenu = targetRef.current;
+    const bar = targetRef2.current;
 
     if (navMenu) {
       if (navMenu.classList.contains("links")) {
         navMenu.classList.remove("links");
         navMenu.classList.add("disappear");
+        bar.classList.remove("change");
       } else {
         navMenu.classList.add("links");
         navMenu.classList.remove("disappear");
+        bar.classList.add("change");
       }
     }
   };
@@ -25,11 +29,11 @@ const Navbar = () => {
         <img className="logo" src={logo} alt="" />
 
         <ul className="links" ref={targetRef}>
-          <li>Home</li>
-          <li>About</li>
-          <li>Menu</li>
-          <li>Awards</li>
-          <li>Contact</li>
+          <li onClick={showMenu}>Home</li>
+          <li onClick={showMenu}>About</li>
+          <li onClick={showMenu}>Menu</li>
+          <li onClick={showMenu}>Awards</li>
+          <li onClick={showMenu}>Contact</li>
           <div className="top-right1">
             <span>Login</span>
             <span className="not">/</span>
@@ -40,7 +44,7 @@ const Navbar = () => {
             </span>
           </div>
         </ul>
-        <div class="bar" onClick={showMenu}>
+        <div class="bar" onClick={showMenu} ref={targetRef2}>
           <div class="bar1"></div>
           <div class="bar2"></div>
           <div class="bar3"></div>
