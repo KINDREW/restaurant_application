e4ed0e4b386784e0004db984004a0906841b4c41
kindrew <kindrew99@gmail.com>
2023-10-28 22:21:24 +0000

    update

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,50 @@
 import "./navbar.css";
 import logo from "../asserts/gericht.png";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 const Navbar = () => {
+  const targetRef = useRef(null);
+
+  const showMenu = () => {
+    const navMenu = targetRef.current;
+
+    if (navMenu) {
+      if (navMenu.classList.contains("links")) {
+        navMenu.classList.remove("links");
+        navMenu.classList.add("disappear");
+      } else {
+        navMenu.classList.add("links");
+        navMenu.classList.remove("disappear");
+      }
+    }
+  };
+
   return (
     <div className="navbar">
       <nav>
         <img className="logo" src={logo} alt="" />
 
-        <ul className="links">
+        <ul className="links" ref={targetRef}>
           <li>Home</li>
           <li>About</li>
           <li>Menu</li>
           <li>Awards</li>
           <li>Contact</li>
+          <div className="top-right1">
+            <span>Login</span>
+            <span className="not">/</span>
+            <span>Register</span>
+            <span className="not">|</span>
+            <span>
+              Book table<table></table>
+            </span>
+          </div>
         </ul>
-
+        <div class="bar" onClick={showMenu}>
+          <div class="bar1"></div>
+          <div class="bar2"></div>
+          <div class="bar3"></div>
+        </div>
         <div className="top-right">
           <span>Login</span>
           <span>/</span>
@@ -24,11 +53,6 @@ const Navbar = () => {
           <span>
             Book table<table></table>
           </span>
-        </div>
-        <div class="bar">
-          <div class="bar1"></div>
-          <div class="bar2"></div>
-          <div class="bar3"></div>
         </div>
       </nav>
     </div>
